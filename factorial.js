aa5e1c6bc8bfff5ce6b252442e7a2905a2f71551
Dan Thackray <djthackray@gmail.com>
2022-09-11 14:46:25 +0100

    Add Big O
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- factorial.js
+++ factorial.js
@@ -1,15 +1,18 @@
 const factorial = (n) => {
-    if (n === 0 || n === 1) {
-        return 1
-    }
+    let result = 1
 
-    let result = 0
-
-    for (let i = n; i = 1; i--) {
-        result += i * i-1
+    for (let i = 2; i <= n; i++) {
+        result = result * i
     }
 
     return result
 }
 
+console.log(factorial(0))
+console.log(factorial(1))
 console.log(factorial(4))
+console.log(factorial(7))
+console.log(factorial(13))
+console.log(factorial(20))
+
+// Big-O = O(n)
