8c78ef4f147163ee29d403b712bb3ccf568c8be0
Dan Thackray <djthackray@gmail.com>
2022-09-11 14:23:15 +0100

    Fibonacci implementation
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+const n = 0
+
+const fibonacci = (n) => {
+    const sequence = [0, 1]
+
+    for (let i = 2; i < n; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2]
+    }
+
+    return sequence
+}
+
+console.log(fibonacci(2))
+console.log(fibonacci(3))
+console.log(fibonacci(7))
+console.log(fibonacci(12))
+console.log(fibonacci(20))
