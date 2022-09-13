eb437a6c2296755598032c97aa4afcc0818a03f4
Dan Thackray <djthackray@gmail.com>
2022-09-13 12:18:42 +0100

    Power of 2 - Bitwise solution
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- powertwo.js
+++ powertwo.js
@@ -1,16 +1,23 @@
-const powertwo = (n) => {
+function isPowerOfTwoBitwise(n) {
     if (n < 1) {
         return false
     }
-
-    while(n > 1) {
-        if (n % 2 !== 0) {
-            return false
-        }
-        n = n / 2
-    }
-
-    return true
+    return (n & (n-1)) === 0
 }
+
+// const powertwo = (n) => {
+//     if (n < 1) {
+//         return false
+//     }
+
+//     while(n > 1) {
+//         if (n % 2 !== 0) {
+//             return false
+//         }
+//         n = n / 2
+//     }
+
+//     return true
+// }
 
 // Big-O = O(logn)
